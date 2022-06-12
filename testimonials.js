f861e340f3d02381a920b6694e42156c9bd64a98
antmar0417 <tonyboy@live.se>
2022-06-12 20:03:32 +0200

    added old projects

--- testimonials.js
+++ testimonials.js
@@ -2,14 +2,14 @@ const testimonials = [
   {
     quote:
       "Anton is a wonderful classmate who works and contributes extremely well in a group. He is always positive and happy to share his thoughts/ideas. Also a good communicator and communicates his solutions in a simple and understandable way.",
-    avatar: "avatar-marcus",
+    avatar: "avatar_marcus",
     name: "Marcus Hägerstrand",
     title: "Web Developer",
   },
   {
     quote:
       "Anton is a kind, ambitious, and organized person who always puts others first. For him, problems are not just challenges, he never backs down, and even in the tough times, he manages to find new opportunities.",
-    avatar: "avatar-natasha",
+    avatar: "avatar_natasha",
     name: "Natasha Petsurova",
     title: "Local Manager",
   },
